aa1842d3d81071d200cb67f5b4c2444e387a42c0
g8rdier <g.kobilarov@purpose.hr>
2024-11-26 01:09:18 +0100

    remove sb

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,23 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-/** @type {import('next-image-export-optimizer').nextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
-  basePath: isProd ? '/website' : '',
+  basePath: '',
   images: {
     loader: 'custom',
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-  transpilePackages: ['next-image-export-optimizer'],
   env: {
-    nextImageExportOptimizer_imageFolderPath: 'public/images',
-    nextImageExportOptimizer_exportFolderPath: 'out',
-    nextImageExportOptimizer_quality: '75',
-    nextImageExportOptimizer_storePicturesInWEBP: 'true',
-    nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
-    nextImageExportOptimizer_generateAndUseBlurImages: 'true',
-    nextImageExportOptimizer_remoteImageCacheTTL: '0',
+    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
   },
 }
 
-export const { basePath } = nextConfig
+export const basePath = nextConfig.basePath
+
 export default nextConfig
